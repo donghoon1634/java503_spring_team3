3cbf457e303fcb8eb45d564621a640d167418e2c
donghoon1634 <tkdduq010@naver.com>
2025-02-10 16:44:12 +0900

    .

---
+++ src/main/java/bitc/fullstack503/java503_team3/Java503Team3Application.java
@@ -0,0 +1,13 @@
+package bitc.fullstack503.java503_team3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java503Team3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Java503Team3Application.class, args);
+    }
+
+}
---
+++ src/test/java/bitc/fullstack503/java503_team3/Java503Team3ApplicationTests.java
@@ -0,0 +1,13 @@
+package bitc.fullstack503.java503_team3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java503Team3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
