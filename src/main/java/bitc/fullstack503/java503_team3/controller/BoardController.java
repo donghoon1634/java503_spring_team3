7fa04844054c6e3c5de2cd9fa60a400ad2fec6c7
Dashboard <zxzx1332@naver.com>
2025-02-11 10:48:40 +0900

    controller 추가

---
+++ src/main/java/bitc/fullstack503/java503_team3/controller/BoardController.java
@@ -0,0 +1,25 @@
+package bitc.fullstack503.java503_team3.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class BoardController {
+
+    @Autowired
+    private BoardService boardService;
+
+    @GetMapping("/board/write")
+    public String insertBoard(){
+        return "board/boardWrite";
+    }
+
+    @PostMapping("/board/write")
+    public String insertBoard(BoardDTO board)throws Exception{
+        boardService.insertBoard(board);
+        return "redirect:/board";
+    }
+}
