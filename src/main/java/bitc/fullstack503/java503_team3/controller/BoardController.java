dfc08325e8588ef57eee26210503b4616255f99e
Dashboard <zxzx1332@naver.com>
2025-02-11 10:52:18 +0900

    boardDTO 추가

--- src/main/java/bitc/fullstack503/java503_team3/controller/BoardController.java
+++ src/main/java/bitc/fullstack503/java503_team3/controller/BoardController.java
@@ -1,5 +1,6 @@
 package bitc.fullstack503.java503_team3.controller;
 
+import bitc.fullstack503.java503_team3.dto.BoardDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
