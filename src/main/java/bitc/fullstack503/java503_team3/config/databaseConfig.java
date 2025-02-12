a9cdfc83d1d4e4ae835734dc1ddc73683f233378
donghoon1634 <tkdduq010@naver.com>
2025-02-12 17:04:12 +0900

    데이터 베이스 연결 완료!

---
+++ src/main/java/bitc/fullstack503/java503_team3/config/databaseConfig.java
@@ -0,0 +1,53 @@
+package bitc.fullstack503.java503_team3.config;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+
+import javax.sql.DataSource;
+
+@Configuration
+@PropertySource("classpath:/application.properties")
+
+public class databaseConfig {
+
+    @Autowired
+    private ApplicationContext applicationContext;
+
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource.hikari")
+    public HikariConfig hikariConfig() {
+
+        return new HikariConfig();
+    }
+
+    @Bean
+    public DataSource dataSource() throws Exception {
+        DataSource ds = new HikariDataSource(hikariConfig());
+        System.out.println(ds.toString());
+        return ds;
+    }
+
+    @Bean
+    public SqlSessionTemplate sqlSessionTemplate(SqlSessionFactory sqlSessionFactory) throws Exception {
+        return new SqlSessionTemplate(sqlSessionFactory);
+    }
+
+    @Bean
+    @ConfigurationProperties(prefix = "mybatis.configuration")
+    public org.apache.ibatis.session.Configuration mybatisConfig() {
+        return new org.apache.ibatis.session.Configuration();
+    }
+
+}
+
+
